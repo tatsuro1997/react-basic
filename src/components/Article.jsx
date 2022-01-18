732d8eef0819835bd3ef419e0e5de840af6311a7
tatsuro1997 <minagawa@dptr.jp>
2022-01-18 21:17:47 +0900

    stateAndProps

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,10 +1,16 @@
-import { Content, Title } from './index';
+import { useState } from 'react';
+import { Content, Title, PublishedButton } from './index';
 
 const Article = (props) => {
+  const [isPublished, setIsPublished] = useState(false)
+  const publishArticle = () => {
+    setIsPublished(true)
+  }
   return (
     <div>
       <Title title={props.title}/>
-      <Content content={props.content}/>
+      <Content content={props.content} />
+      <PublishedButton isPublished={isPublished} onClick={publishArticle} />
     </div>
   );
 };
