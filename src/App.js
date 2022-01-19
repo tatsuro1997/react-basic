ed1e2f691be5a44a96d9503efd213a0a00c426d8
tatsuro1997 <minagawa@dptr.jp>
2022-01-19 16:54:03 +0900

    fetch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react";
 import Article from "./components/Article";
 
 function App() {
+  const [name, setName] = useState('')
+  const [id, setId] = useState('tatsuro1997')
+  const ids = ['tatsuro1997','aws', 'facebook']
+  const getRandomId = () => {
+    const _id = ids[Math.floor(Math.random() * ids.length)]
+    setId(_id)
+  }
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${id}`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setName(data.name)
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  },[id])
+
   return (
     <div>
-      <Article
+      <p>{id}の、GitHub上の名前は{name}です。</p>
+      <button onClick={getRandomId}>IDを変更</button>
+      {/* <Article
         title={'タイトル'}
         content={'コンテンツ'}
       />
@@ -14,7 +37,7 @@ function App() {
       <Article
         title={'再利用可能'}
         content={'コンテンツ'}
-      />
+      /> */}
     </div>
   );
 };
